c135b88fb4e6cc8150f9d9a94f8415999badf41f
Joseph <joseph161216@gmail.com>
2023-06-28 23:10:32 -0700

    Add sidebar with header

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import SidebarWithHeader from "@/components/SidebarWithHeader/SidebarWithHeader";
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <>
+      <SidebarWithHeader>
+        <h1>Hello, SidebarWithHeader!</h1>
+        <p>This is the content of the SidebarWithHeader.</p>
+      </SidebarWithHeader>
+    </>
+  );
+}
