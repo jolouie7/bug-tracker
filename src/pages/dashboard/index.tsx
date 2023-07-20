f0dc5df08f44a7f468345ea31b43dcf65cfa295d
Joseph <joseph161216@gmail.com>
2023-07-20 13:33:34 -0700

    Add bar chart

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,12 @@
 import SidebarWithHeader from "@/components/SidebarWithHeader/SidebarWithHeader";
-import React from "react";
+import Bar from "../../components/Bar";
 
 export default function Dashboard() {
   return (
     <>
       <SidebarWithHeader>
         <h1>Hello, SidebarWithHeader!</h1>
-        <p>This is the content of the SidebarWithHeader.</p>
+        <Bar />;
       </SidebarWithHeader>
     </>
   );
