8718e4148ca01a3cc820fcc078698c06b94cbdfb
Joseph <joseph161216@gmail.com>
2023-07-20 19:47:37 -0700

    Add dashboard content

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,27 @@
 import SidebarWithHeader from "@/components/SidebarWithHeader/SidebarWithHeader";
 import Bar from "../../components/Bar";
+import { Box, Center, Flex } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
     <>
       <SidebarWithHeader>
         <h1>Hello, SidebarWithHeader!</h1>
-        <Bar />;
+        <Flex flexWrap="wrap" flexGrow={1}>
+          {/* Four equal squares */}
+          <Box w={["100%", "50%"]} p={4}>
+            <Bar />
+          </Box>
+          <Box w={["100%", "50%"]} p={4}>
+            <Bar />
+          </Box>
+          <Box w={["100%", "50%"]} p={4}>
+            <Bar />
+          </Box>
+          <Box w={["100%", "50%"]} p={4}>
+            <Bar />
+          </Box>
+        </Flex>
       </SidebarWithHeader>
     </>
   );
