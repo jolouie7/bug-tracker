a8bfc84fe00d3e0aa3a6610837bee9f6d69b30bd
Joseph <joseph161216@gmail.com>
2023-09-02 23:33:21 -0700

    Add private routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,39 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
 import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
+
+import Signin from "./signin";
+
+if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
+  throw "Missing Publishable Key";
+}
+
+const clerkPubKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const publicPages = ["/signin", "/signup"];
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+  const isPublicPage = publicPages.includes(pathname);
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <ChakraProvider>
+        {isPublicPage ? (
+          <Component {...pageProps} />
+        ) : (
+          <>
+            <SignedIn>
+              <Component {...pageProps} />
+            </SignedIn>
+            <SignedOut>
+              <Signin />
+            </SignedOut>
+          </>
+        )}
+      </ChakraProvider>
+    </ClerkProvider>
   );
 }
