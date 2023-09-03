b09fb26664b0a1baa0d30bf3f38a53f430b784d6
Joseph <joseph161216@gmail.com>
2023-09-02 23:33:54 -0700

    Add Home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { useUser } from "@clerk/nextjs";
+
 export default function Home() {
+  // Use the useUser hook to get the Clerk.user object
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+
+  console.log(user);
+
   return (
     <>
-      <main>Hello World!!</main>
+      <main>Hello {user.primaryEmailAddress?.emailAddress}!!</main>
     </>
   );
 }
